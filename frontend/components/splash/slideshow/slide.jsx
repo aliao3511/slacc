3095462142a0375179e229e15e074308b047e299
al3511 <alexandraliao3511@gmail.com>
2019-07-31 17:35:17 -0400

    Enable slideshow and navbar

--- frontend/components/splash/slideshow/slide.jsx
+++ frontend/components/splash/slideshow/slide.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
 const Slide = ({ image }) => {
+    // this does not work
+    // const style = {
+    //     backgroundImage: `url(${image})`,
+    //     backgroundRepeat: 'no-repeat',
+    //     backgroundSize: 'cover',
+    // }
+
     return (
+        // <div className="slide" style={style}>
         <div className="slide">
-            <img src={`assets/${image}`} alt=""/>
+            {/* this works */}
+            <img src={`assets/${image}`} alt=""/> 
         </div>
     );
 };
--- frontend/components/splash/splash_container.jsx
+++ frontend/components/splash/splash_container.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import SignupFormContainer from '../session/signup_form_container';
 import { Link } from 'react-router-dom';
+import Slideshow from './slideshow/slideshow';
 
 const mapStateToProps = state => ({
     currentUser: state.entities.users[state.session.id],
@@ -11,7 +12,6 @@ class Splash extends React.Component {
 
     render() {
         const { currentUser } = this.props;
-        console.log(currentUser);
         const splashText = currentUser ? (
             <>
                 <div className="splash-byline">
@@ -37,35 +37,42 @@ class Splash extends React.Component {
             </>
         );
 
-        console.log(splashText);
         return (
             <div className="splash-container">
-                <div className="splash-text">
-                    {splashText}
+                <div className="splash-top">
+
+                    <div className="splash-text">
+                        {splashText}
+                    </div>
+
+                    <div className="splash-animation">
+                        <img className="dropbox" src={'assets/dropbox.png'} alt="dropbox"/>
+                        <img className="asana" src={'assets/asana.png'} alt="asana"/>
+                        <img className="googledrive" src={'assets/googledrive.png'} alt="google"/>
+                        <img className="salesforce" src={'assets/salesforce.png'} alt="salesforce"/>
+                        <img className="jira" src={'assets/jira.png'} alt="jira"/>
+                        <img className="zendesk" src={'assets/zendesk.png'} alt="zendesk"/>
+                        <img className="gotomeeting" src={'assets/gotomeeting.png'} alt="gotomeeting"/>
+                        <img className="onedrive" src={'assets/onedrive.png'} alt="onedrive"/>
+                        <img className="zoom" src={'assets/zoom.png'} alt="zoom"/>
+                        <img className="giphy" src={'assets/giphy.png'} alt="giphy"/>
+                        <img className="lattice" src={'assets/lattice.png'} alt="lattice"/>
+                        <img className="teamline" src={'assets/teamline.png'} alt="teamline"/>
+                        <img className="dash" src={'assets/dash.png'} alt="dash"/>
+                        <img className="outlook" src={'assets/outlook.png'} alt="outlook"/>
+                        <img className="cisco" src={'assets/cisco.png'} alt="cisco"/>
+                        <img className="loom" src={'assets/loom.png'} alt="loom"/>
+                    </div>
+
                 </div>
-                <div className="splash-animation">
-                    <img className="dropbox" src={'assets/dropbox.png'} alt="dropbox"/>
-                    <img className="asana" src={'assets/asana.png'} alt="asana"/>
-                    <img className="googledrive" src={'assets/googledrive.png'} alt="google"/>
-                    <img className="salesforce" src={'assets/salesforce.png'} alt="salesforce"/>
-                    <img className="jira" src={'assets/jira.png'} alt="jira"/>
-                    <img className="zendesk" src={'assets/zendesk.png'} alt="zendesk"/>
-                    <img className="gotomeeting" src={'assets/gotomeeting.png'} alt="gotomeeting"/>
-                    <img className="onedrive" src={'assets/onedrive.png'} alt="onedrive"/>
-                    <img className="zoom" src={'assets/zoom.png'} alt="zoom"/>
-                    <img className="giphy" src={'assets/giphy.png'} alt="giphy"/>
-                    <img className="lattice" src={'assets/lattice.png'} alt="lattice"/>
-                    <img className="teamline" src={'assets/teamline.png'} alt="teamline"/>
-                    <img className="dash" src={'assets/dash.png'} alt="dash"/>
-                    <img className="outlook" src={'assets/outlook.png'} alt="outlook"/>
-                    <img className="cisco" src={'assets/cisco.png'} alt="cisco"/>
-                    <img className="loom" src={'assets/loom.png'} alt="loom"/>
-                </div>
+                    <section className="splash-section-1">
+                        <h2>See slacc go</h2>
+                        <Slideshow />
+                    </section>
             </div>
         )
     }
    
 }
 
-// export default SplashContainer;
 export default connect(mapStateToProps)(Splash);
