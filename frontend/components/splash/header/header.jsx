b8862361949649dab2558cc8033ed6c7844acba2
al3511 <alexandraliao3511@gmail.com>
2019-07-31 17:39:29 -0400

    Resolve branch conflict

--- frontend/components/splash/header/header.jsx
+++ frontend/components/splash/header/header.jsx
@@ -9,35 +9,12 @@ class Header extends React.Component {
                 <button className="button" onClick={logout}>log out</button>
             </>
         ) : (
-<<<<<<< HEAD
-                <>
-                    <div className="left">
-                        <div className="logo">
-                            {/* <img src={'assets/logo.png'} alt="logo" /> */}
-                            <img src={window.logoURL} alt="logo" />
-                            <p>slacc</p>
-                        </div>
-                        <div className="credits">
-                            <a href="https://github.com/aliao3511">
-                                {/* <img src={'assets/github_logo.png'} alt="github"/> */}
-                                <img src={window.githubURL} alt="github"/>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="header-buttons">
-                        <Link to="/login">Sign in</Link>
-                        <Link to="/signup" className="button">Get Started</Link>
-                    </div>
-                </>
-            );
-=======
             <>
                 <Link to="/login">Sign in</Link>
                 <Link to="/signup" className="button">Get Started</Link>
             </>
         );
 
->>>>>>> user_auth
         return (
             <div className="header-content">
                 <div className="left">
